11d05a5b608f38b2681071caa0e9287b2b6ad70c
Roel Vaneyghen <roel.vaneyghen@gmail.com>
2018-05-07 17:59:41 +0700

    add options form to add telegram tokens

--- background.js
+++ background.js
@@ -1,18 +1,20 @@
 const xhr = new XMLHttpRequest();
+let TOKEN = '';
+let CHAT_ID = '';
+
+chrome.storage.sync.get('telegramSettings', data => {
+  const { token, chatId } = data.telegramSettings;
+  TOKEN = token || '';
+  CHAT_ID = chatId || '';
+});
 
 chrome.browserAction.onClicked.addListener(tab => {
-  xhr.open(
-    'POST',
-    'https://api.telegram.org/bot517613587:AAHXrJJ1p26De6eDbblS_EWAgr4AoYhElrw/sendMessage',
-    true
-  );
+  xhr.open('POST', `https://api.telegram.org/bot${TOKEN}/sendMessage`, true);
   xhr.setRequestHeader('Content-type', 'application/json');
   xhr.send(
     JSON.stringify({
       text: tab.url,
-      chat_id: '386351095'
+      chat_id: CHAT_ID
     })
   );
-  const result = xhr.responseText;
-  console.log(result);
 });
